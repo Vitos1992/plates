4e9de5141dc3700c38d7708d60c96d2766417407
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2025-02-04 18:53:31 +0200

    new

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
-$(document).ready(function(){
-    $('.slider_clik').slick();
-});
+
+// /---------------section_six-----------------/ 
 
 const contBox =document.querySelector('.cont_box');
 const sliderLlineComp = document.querySelectorAll('.slider_line_comp');
 const sliderRow = document.querySelector('.slider_row');
+
+const sliderBtnLeft = document.querySelector('.slider_btn_left');
+const sliderBtnRight = document.querySelector('.slider_btn_right');
+
+let sliderCount = 0;
+let sliderWidth = contBox.affsetWidth;
+
+// перемотка слайда у перед при натиску кнопки NEXT
+sliderBtnRight.addEventListener('click', nextSlide);
+
+//перемотка слайда у перед при натиску кнопки NEXT
+sliderBtnLeft.addEventListener('click', leftSlider);
+
+function nextSlide() {
+    sliderCount++;
+    console.log(sliderCount);
+
+    rollSlider();
+}
+
+function leftSlider() {
+    sliderCount--;
+    console.log(sliderCount);
+
+    rollSlider();
+}
+
+function rollSlider() {
+    sliderRow.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+}
