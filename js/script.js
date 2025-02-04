211ba324ca02bbdf09cc059a4f151dd045985c0f
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2025-02-04 15:42:42 +0200

    newwq

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
     $('.slider_clik').slick();
 });
+
+const contBox =document.querySelector('.cont_box');
+const sliderLlineComp = document.querySelectorAll('.slider_line_comp');
+const sliderRow = document.querySelector('.slider_row');
