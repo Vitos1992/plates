15ebf2482ddc90a2296e3895b7db4cdc2366e419
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2025-02-06 22:23:18 +0200

    nert

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@ $(document).ready(function(){
     $('.slick_role').slick({
         arrows:true,
         dots:true,
+        adaptiveHeight:true,
+        speed:1000,
+        easing:'ease',
+    });
+});
+
+$(document).ready(function(){
+    $('.slider_row').slick({
+        // arrows:true,
+        // dots:true,
         // adaptiveHeight:true,
         // speed:1000,
         // easing:'ease',
