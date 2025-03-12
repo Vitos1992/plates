aa205afd405713ceafaea9ed27097f92b6d2bc12
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2025-03-12 12:27:40 +0200

    new

--- js/script.js
+++ js/script.js
@@ -54,3 +54,26 @@ $(".slider_line").slick({
     refs.body.classList.toggle("no-scroll");
     }
 })();
+
+// menu_mobile
+
+(() => {
+    const refs = {
+      openMenuBtn: document.querySelector("[data-menu-open]"),
+      closeMenuBtn: document.querySelector("[data-menu-close]"),
+      menu: document.querySelector("[data-menu]"),
+    };
+  
+    refs.openMenuBtn.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  
+    function toggleMenu() {
+        refs.menu.classList.toggle("is-hidden");
+    }
+  })();
+
+  document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burger").addEventListener("click", function() {
+        document.querySelector("open_mobile").classList.toggle("burger_close")
+    })
+  })
